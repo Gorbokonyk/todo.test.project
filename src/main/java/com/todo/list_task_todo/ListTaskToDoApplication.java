21dbe8594f42a3251a10e5f2839f1c8f60771a6f
OlehOnyshchuk <oleh@phokus.io>
2024-09-22 11:22:11 +0300

    Create project, add liquibase migration, add h2db

---
+++ src/main/java/com/todo/list_task_todo/ListTaskToDoApplication.java
@@ -0,0 +1,13 @@
+package com.todo.list_task_todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListTaskToDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListTaskToDoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/todo/list_task_todo/ListTaskToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todo.list_task_todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListTaskToDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
