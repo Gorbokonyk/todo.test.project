ba953f1a10c21f94bc93a20bffd05b11f0ed0c26
OlehOnyshchuk <oleh@phokus.io>
2024-10-28 19:40:18 +0200

    fix code

--- src/main/java/com/todo/list_task_todo/file/model/FileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.todo.list_task_todo.file.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface FileRepository extends JpaRepository<FileEntity, Long>, JpaSpecificationExecutor<FileEntity> {
-}
